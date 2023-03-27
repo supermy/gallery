ba7364b15e47696c2ccf79088bc90731c52d3537
supermy <springclick@gmail.com>
2023-03-27 23:09:51 +0800

    定制登录页面

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ void main() {
 class GalleryApp extends StatelessWidget {
   const GalleryApp({
     super.key,
-    this.initialRoute,
+    this.initialRoute='/shrine',
     this.isTestMode = false,
   });
 
@@ -48,8 +48,9 @@ class GalleryApp extends StatelessWidget {
         builder: (context) {
           final options = GalleryOptions.of(context);
           return MaterialApp(
+            //应用程序状态恢复的标识符
             restorationScopeId: 'rootGallery',
-            title: 'Flutter Gallery',
+            title: 'Flutter 展览馆',
             debugShowCheckedModeBanner: false,
             themeMode: options.themeMode,
             theme: GalleryThemeData.lightThemeData.copyWith(
@@ -62,6 +63,10 @@ class GalleryApp extends StatelessWidget {
               ...GalleryLocalizations.localizationsDelegates,
               LocaleNamesLocalizationsDelegate()
             ],
+            //如果提供了 initialRoute ，则用于生成初始路由的路由生成器回调，如果未设置此属性，
+            // 则底层 Navigator.onGenerateInitialRoutes 将默认为 Navigator.defaultGenerateInitialRoutes。
+
+            //初始路由，如果设置了该参数并且在 routes 找到了对应的key，将会展示对应的 Widget ，否则展示 home
             initialRoute: initialRoute,
             supportedLocales: GalleryLocalizations.supportedLocales,
             locale: options.locale,
@@ -69,6 +74,8 @@ class GalleryApp extends StatelessWidget {
               deviceLocale = locales?.first;
               return basicLocaleListResolution(locales, supportedLocales);
             },
+            //路由管理拦截器
+            //当跳转路由时，如果在 routes 找不到对应的 key ，会执行该回调，会调用会返回一个 RouteSettings ，该对象中有 name 路由名称、 arguments 路由参数。
             onGenerateRoute: RouteConfiguration.onGenerateRoute,
           );
         },
